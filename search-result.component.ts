f993084697061e67e16944f8710e0089ae070a8e
chaimabenmoallem <76392216+chaimabenmoallem@users.noreply.github.com>
2023-09-23 13:14:00 +0100

    Update search-result.component.ts

--- search-result.component.ts
+++ search-result.component.ts
@@ -24,6 +24,57 @@ export class SearchResultComponent {
       
       // Log the imageUrl from the carDetails
       this.carDetails.forEach((detail:any) => {
+        if (detail.imageUrl) {
+          detail.imageUrl = detail.imageUrl.replace(' ', '%20');
+          
+      }
+      
+    });
+    });
+  }
+
+  close(){
+    this.router.navigate(['/cars'])
+  }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+/*import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SharedDataService } from 'src/app/services/shared-data.service'
+@Component({
+  selector: 'app-search-result',
+  templateUrl: './search-result.component.html',
+  styleUrls: ['./search-result.component.css']
+})
+export class SearchResultComponent {
+  imageUrl: string | null = null;
+  carDetails: any = {};
+
+  constructor(private sharedDataService: SharedDataService,
+    private router: Router ) { }
+
+  ngOnInit(): void {
+    this.sharedDataService.currentImageUrl.subscribe(imageUrl => {
+      this.imageUrl = imageUrl;
+    });
+    
+    this.sharedDataService.currentCarDetails.subscribe(details => {
+      console.log("Received car details:", details);
+      this.carDetails = details;
+      
+      // Log the imageUrl from the carDetails
+      this.carDetails.forEach((detail:any) => {
         detail.imageUrl = detail.imageUrl.replace(' ', '%20');
         console.log(detail.imageUrl);
     });
@@ -34,3 +85,4 @@ export class SearchResultComponent {
     this.router.navigate(['/cars'])
   }
   }
+*/
